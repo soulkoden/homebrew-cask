fffba0838042c9887358e51bc1c16afc31823279
Sergey Kodentsov <soulkoden@gmail.com>
2025-03-22 09:59:30 +0300

    🔧 chore(Casks/q/quicktone.rb): adjust cask name to match convention

--- Casks/q/quicktone.rb
+++ Casks/q/quicktone.rb
@@ -1,9 +1,9 @@
-cask "quick-tone" do
+cask "quicktone" do
     version "2.4.12.27"
     sha256 "7f7e5399f9783cfdc541f26ac2a9632956fca0ef453013c1458b5c10c0a17ce0"
   
     url "https://nux.cherubtechnology.com//enclosure/sources/rHsTpBFUWRxDQNcT/NUX_QuickTone_MacOS_V#{version}.zip"
-    name "quick-tone"
+    name "quicktone"
     desc "Quick Tone for FW V5"
     homepage "https://www.nuxaudio.com/home.html"
   
